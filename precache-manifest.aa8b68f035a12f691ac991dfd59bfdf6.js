2c21801a9d4f740b25151ec93bc72938ae743410
Jay Li <jay.li23697@gmail.com>
2019-04-13 00:03:40 +0800

    Updates

---
+++ precache-manifest.aa8b68f035a12f691ac991dfd59bfdf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "5753a8e31c8695f14818",
+    "url": "/robo-friends/static/js/main.80a71072.chunk.js"
+  },
+  {
+    "revision": "b1cb01f647e932aadfdf",
+    "url": "/robo-friends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "5753a8e31c8695f14818",
+    "url": "/robo-friends/static/css/main.37b436e1.chunk.css"
+  },
+  {
+    "revision": "b1cb01f647e932aadfdf",
+    "url": "/robo-friends/static/css/2.f66759e4.chunk.css"
+  },
+  {
+    "revision": "37f8f3d0f2f53a2f0e7c35cfd4ec9f5e",
+    "url": "/robo-friends/index.html"
+  }
+];
